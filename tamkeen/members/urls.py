15275f68eff3aefa2102feb63b9b8483895aa8a5
aelbarbary <abde.elbarbary@AV-MBP-3564.local>
2017-01-16 15:55:36 -0800

    add css and images

--- tamkeen/members/urls.py
+++ tamkeen/members/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = 'members'
 urlpatterns = [
     url(r'^$', views.index, name='index')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
