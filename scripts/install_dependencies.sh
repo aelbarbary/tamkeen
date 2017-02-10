589eddc10136d635ff12c062b8ca28fc55649d7c
aelbarbary <abde.elbarbary@AV-MBP-3564.local>
2017-02-10 14:40:41 -0800

    codedeploy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install nano
 sudo apt-get install python3-setuptools
+sudo apt-get install libpq-dev
 sudo easy_install3 pip
 pip3 install Django
 pip3 install psycopg2
