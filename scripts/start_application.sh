cca6b5b9be7222cdef3c5553eab8988e2c218958
aelbarbary <abde.elbarbary@AV-MBP-3564.local>
2017-06-01 12:17:00 -0700

    fix deployment

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,5 @@
 # sudo python3 /home/ubuntu/www/tamkeen/app/manage.py runserver 0.0.0.0:80 > /dev/null 2>&1 &
-# cd /home/ubuntu/www/tamkeen/app && uwsgi --socket :8003 --module tamkeen.wsgi
 sudo systemctl start nginx
+
+sudo kill -9 $(sudo lsof -t -i:8003)
+cd /home/ubuntu/www/tamkeen/app && uwsgi --socket :8003 --module tamkeen.wsgi
