ccf3b7fcc5f0ab8b19075fd16f533d3ca6fc200f
aelbarbary <abde.elbarbary@AV-MBP-3564.local>
2017-06-01 11:49:44 -0700

    fix build

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,3 @@
 # sudo python3 /home/ubuntu/www/tamkeen/app/manage.py runserver 0.0.0.0:80 > /dev/null 2>&1 &
-cd /home/ubuntu/www/tamkeen/app && uwsgi --socket :8003 --module tamkeen.wsgi
+# cd /home/ubuntu/www/tamkeen/app && uwsgi --socket :8003 --module tamkeen.wsgi
 sudo systemctl start nginx
