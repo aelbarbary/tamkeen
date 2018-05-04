13055dc24369d71574151cf01bbbd4d404ab9d5c
aelbarbary <abdelrahman.elbarbary@gmail.com>
2018-05-04 12:44:59 -0700

    undo codedeploy scripts

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,5 +1,5 @@
 # sudo python3 /home/ubuntu/www/tamkeen/app/manage.py runserver 0.0.0.0:80 > /dev/null 2>&1 &
-sudo systemctl restart nginx
+sudo systemctl start nginx
 
 sudo kill -9 $(sudo lsof -t -i:8003)
 sudo uwsgi --chdir /home/ubuntu/www/tamkeen/app --socket :8003 --module tamkeen.wsgi > /dev/null 2>&1 &
